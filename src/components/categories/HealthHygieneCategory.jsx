5e6b8940747c517c579c06b13fca22a0dd2939b2
Rafid Ahmed <rafidahmed43@gmail.com>
2025-08-05 22:05:57 +0600

    Added some more medicine

--- src/components/categories/HealthHygieneCategory.jsx
+++ src/components/categories/HealthHygieneCategory.jsx
@@ -8,21 +8,93 @@ const HealthHygieneCategory = () => {
   const dummyProducts = [
     {
       id: 'hyg1',
-      name: 'Hand Sanitizer',
-      description: 'Alcohol-based hand sanitizer with moisturizing formula',
-      price: 199.99,
-      image: '/images/products/product-1.jpg',
-      stock: 150,
-      manufacturer: 'CleanHands'
+      name: 'Dettol Hand Sanitizer',
+      description: 'Instant Hand Sanitizer with Moisturizer (200ml)',
+      price: 160.00,
+      image: '/images/products/product-4.jpg',
+      stock: 200,
+      manufacturer: 'Reckitt Benckiser'
     },
     {
       id: 'hyg2',
-      name: 'Face Masks Pack',
-      description: 'Pack of 50 disposable 3-ply face masks',
-      price: 499.99,
+      name: 'Savlon Antiseptic',
+      description: 'Antiseptic Liquid for cuts and wounds (500ml)',
+      price: 140.00,
+      image: '/images/products/product-1.jpg',
+      stock: 180,
+      manufacturer: 'ICI'
+    },
+    {
+      id: 'hyg3',
+      name: 'Betadine Solution',
+      description: 'Povidone-Iodine Antiseptic Solution (100ml)',
+      price: 185.00,
       image: '/images/products/product-2.jpg',
-      stock: 100,
-      manufacturer: 'SafeBreath'
+      stock: 120,
+      manufacturer: 'Mundipharma'
+    },
+    {
+      id: 'hyg4',
+      name: 'Paracetamol 500mg',
+      description: 'Fever and Pain Relief Tablets (10 tablets/strip)',
+      price: 45.00,
+      image: '/images/products/product-3.jpg',
+      stock: 250,
+      manufacturer: 'Incepta Pharmaceuticals'
+    },
+    {
+      id: 'hyg5',
+      name: 'Vitamin C 500mg',
+      description: 'Immune Support Supplement (30 tablets/bottle)',
+      price: 120.00,
+      image: '/images/products/product-4.jpg',
+      stock: 180,
+      manufacturer: 'Healthcare Nutrition'
+    },
+    {
+      id: 'hyg6',
+      name: 'First Aid Antiseptic Spray',
+      description: 'Quick-Drying No-Sting Antiseptic Spray (120ml)',
+      price: 220.00,
+      image: '/images/products/product-1.jpg',
+      stock: 95,
+      manufacturer: 'Johnson & Johnson'
+    },
+    {
+      id: 'hyg7',
+      name: 'Ibuprofen 400mg',
+      description: 'Anti-inflammatory Pain Relief Tablets (10 tablets/strip)',
+      price: 65.00,
+      image: '/images/products/product-2.jpg',
+      stock: 180,
+      manufacturer: 'Square Pharmaceuticals'
+    },
+    {
+      id: 'hyg8',
+      name: 'Multivitamin Tablets',
+      description: 'Daily Essential Vitamins & Minerals (60 tablets/bottle)',
+      price: 280.00,
+      image: '/images/products/product-3.jpg',
+      stock: 120,
+      manufacturer: 'Nature\'s Way'
+    },
+    {
+      id: 'hyg9',
+      name: 'Hydrogen Peroxide 3%',
+      description: 'First Aid Antiseptic Solution (100ml)',
+      price: 75.00,
+      image: '/images/products/product-4.jpg',
+      stock: 150,
+      manufacturer: 'MediCare'
+    },
+    {
+      id: 'hyg10',
+      name: 'Allergy Relief Tablets',
+      description: 'Fast-Acting Antihistamine (10 tablets/strip)',
+      price: 95.00,
+      image: '/images/products/product-1.jpg',
+      stock: 130,
+      manufacturer: 'Beximco Pharma'
     }
   ];
 
